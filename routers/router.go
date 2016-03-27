253d4be491b0bcb4801a0516b3e1c712e52579bb
WYL53 <843794313@qq.com>
2016-03-27 17:45:49 +0800

    创建首页

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,11 @@
 package routers
 
 import (
-	"bBlog/controllers"
+	"beegoBlog/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
